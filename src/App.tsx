08da1dd971c72e3dcac9c35cd55c23f844fbf6d6
feibowei <1906524955@qq.com>
2022-01-12 16:04:26 +0800

    feat: js -> ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 import styles from './App.less';
 import { Content } from './components/Content';
 import { Header } from './components/Header';
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,74 +1,21 @@
-export declare global {
-
-  declare module '*.tmLanguage';
-
-  declare module '*.less' {
-    const content: any;
-    export default content;
-  }
-
-  declare module '*.scss' {
-    const content: any;
-    export default content;
-  }
-
-  declare module '*.json' {
-    const value: any;
-    export default value;
-  }
-
-  declare module 'eth-lightwallet/dist/lightwallet.min.js' {
-    const content: any;
-    export default content;
-  }
-
-  declare module '*.png' {
-    const content: any;
-    export default content;
-  }
-
-  declare module '*.svg' {
-    const content: any;
-    export default content;
-  }
-
-  declare module 'showdown-highlight';
-
-  declare module 'react-split';
-
-  declare module '@amplitude/react-amplitude';
-
-  declare module 'semver/ranges/min-version';
-  declare module 'semver/ranges/max-satisfying' {
-    export default function semverMaxVersion(
-      versions: ReactText[],
-      versionRange: string
-    ): string;
-  }
-
-  import { provider } from 'web3-core';
-  import * as ConfluxJSSDK from 'js-conflux-sdk';
-  import Web3 from 'web3';
-  import System from 'System';
-  const { Conflux: ConfluxJS } = ConfluxJSSDK;
-  interface Window {
-    Web3: Web3;
-    pluginStore: any;
-    pluginManager: any;
-    PubSub: any;
-    ethereum: provider;
-    BinanceChain: provider;
-    conflux: provider;
-    confluxJS: ConfluxJS;
-    ConfluxJSSDK: ConfluxJSSDK;
-    System: System;
-  }
-
-  declare function showLoading__GLOBAL(params?: {
-    loadingText?: string;
-    stepCount?: number;
-    currentStep?: number;
-    stepText?: string;
-  }): void;
-  declare function hideLoading__GLOBAL(): void;
+declare module '*.less' {
+  const content: any;
+  export default content;
 }
+
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+
+declare module '*.png' {
+  const content: any;
+  export default content;
+}
+
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+
+
